967c9ae36f0a574506655a57b76bf893a6d697b9
kPatch <irvsteve@gmail.com>
2015-07-09 13:14:38 -0400

    Pushing Eclipse files

---
+++ src/discoverylab/telebot/slave/voice/TelebotSlaveVoice.java
@@ -0,0 +1,5 @@
+package discoverylab.telebot.slave.voice;
+
+public class TelebotSlaveVoice {
+
+}
---
+++ src/discoverylab/telebot/slave/voice/test/TelebotSlaveVoiceTest.java
@@ -0,0 +1,7 @@
+package discoverylab.telebot.slave.voice.test;
+
+public class TelebotSlaveVoiceTest {
+	public static void main(String arg[]){
+		
+	}
+}
